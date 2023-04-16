2625801a2d927927ab8b8ee28d6a329c9473a449
ramma.fitra <rammahayufitra@gmail.com>
2023-04-16 22:57:43 +0700

    OOP part1

---
+++ OOP/classes_objects.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+class YouTubeChannel{
+    public:
+        std::string Name;
+        std::string OwnerName; 
+        int SubscriberCount; 
+        std::list<std::string> PublishedVideoTitles;
+};
+
+int main(){
+    YouTubeChannel ytChannel; 
+    ytChannel.Name = "C++.channel";
+    ytChannel.OwnerName = "Mr.Low Lever"; 
+    ytChannel.SubscriberCount = 1800; 
+    ytChannel.PublishedVideoTitles = {"part a", "part b", "part c"}; 
+
+    std::cout << "Name : " << ytChannel.Name << std::endl;
+    std::cout << "OwnerName : " << ytChannel.OwnerName << std::endl;
+    std::cout << "SubscriberCount : " << ytChannel.SubscriberCount << std::endl; 
+    for (std::string videoTitle: ytChannel.PublishedVideoTitles){
+        std::cout << videoTitle << std::endl;
+    }
+    return 0;
+}
